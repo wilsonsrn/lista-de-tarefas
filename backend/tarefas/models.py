1bf124f808c12f66c1913286c584b863357a3fb8
Wilson Rocha Neto <wilsonsrochaneto@gmail.com>
2024-11-03 18:21:31 -0300

    feat: Adiciona model Tarefa

--- backend/tarefas/models.py
+++ backend/tarefas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Tarefa(models.Model):
+    nome = models.CharField(max_length=255, unique=True)
+    custo = models.DecimalField(max_digits=10, decimal_places=2)
+    data_limite = models.DateField()
+    ordem = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return self.nome
