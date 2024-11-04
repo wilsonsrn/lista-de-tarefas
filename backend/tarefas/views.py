90e9da93784f76b4ff14cc4c35e2f51b995b961a
Wilson Rocha Neto <wilsonsrochaneto@gmail.com>
2024-11-04 10:45:18 -0300

    feat: Adiciona viewset do model Tarefa

--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Tarefa
+from .serializers import TarefaSerializer
 
-# Create your views here.
+
+class TarefaViewSet(viewsets.ModelViewSet):
+    queryset = Tarefa.objects.all().order_by("ordem")
+    serializer_class = TarefaSerializer
+
+    def create(self, request, *args, **kwargs):
+        nome = request.data.get("nome")
+        if Tarefa.objects.filter(nome=nome).exists():
+            return Response(
+                {"error": "Nome da tarefa precisa ser única"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return super().create(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        nome = request.data.get("nome")
+        if Tarefa.objects.filter(nome=nome).exclude(id=kwargs["pk"]).exists():
+            return Response(
+                {"error": "Nome da tarefa precisa ser única"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return super().update(request, *args, **kwargs)
