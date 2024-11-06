607ec8b72208f9ea85b5c47f8eddb5029cf267ba
Wilson Rocha Neto <wilsonsrochaneto@gmail.com>
2024-11-06 13:46:48 -0300

    build: Adiciona a settings cors

--- backend/lista_de_tarefas_api/settings.py
+++ backend/lista_de_tarefas_api/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 # Application definition
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
+    "corsheaders",
     "rest_framework",
     "tarefas",
 ]
@@ -50,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "lista_de_tarefas_api.urls"
