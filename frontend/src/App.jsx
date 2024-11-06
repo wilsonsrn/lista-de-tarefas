443f42a1f6bd316d9e0c28f449882109b4a64541
Wilson Rocha Neto <wilsonsrochaneto@gmail.com>
2024-11-06 14:53:43 -0300

    test: Teste de tema Dracula

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,10 @@ function App() {
     <h1 className="text-2xl text-white font-bold">
       Hello Vite + React + Tailwind!
     </h1>
+    <div class="bg-dracula-pink">
+    <p class="text-dracula-buffy">I vant to suck your blood...</p>
+    <p class="text-pink-500">Tailwind is cool...</p> //still works!
+</div>
     </>
   )
 }
